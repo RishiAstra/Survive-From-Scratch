b7e2cdf1c97c6ca4f50e6502994e77e3fc326a96
Rishi Astra <40644327+bobthefroggywoggy@users.noreply.github.com>
2021-02-25 14:09:09 -0500

    added fullscreen toggle

---
+++ Assets/FullScreenToggle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Toggle()
+	{
+		Screen.fullScreen = !Screen.fullScreen;
+	}
+}
