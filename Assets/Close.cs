64a6ba3b31a1d04150c2ae564eeccbe2af5cd8f5
Rishi Astra <40644327+bobthefroggywoggy@users.noreply.github.com>
2021-02-27 15:04:54 -0500

    added some buttons

---
+++ Assets/Close.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMenu : MonoBehaviour
+{
+	public GameObject menu;
+    // Start is called before the first frame update
+    void Toggle()
+	{
+		menu.SetActive(!menu.activeSelf);
+	}
+}
---
+++ Assets/Scripts/VersionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class VersionHandler : MonoBehaviour
+{
+	public string versionPath
+	{
+		get { return Application.streamingAssetsPath + "/Versions/"; }
+	}
+	public string versionFilePath
+	{
+		get { return Application.streamingAssetsPath + "/Versions/" + version + ".txt"; }
+	}
+
+	public string version;
+	public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+		if (text != null && File.Exists(versionFilePath))
+		{
+			text.text = File.ReadAllText(versionFilePath);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
