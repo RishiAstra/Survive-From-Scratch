d4d43851d3bebd69a57635a193ce747b436a2763
Rishi Astra <40644327+RishiAstra@users.noreply.github.com>
2021-05-05 12:21:37 -0400

    finished basic dialogue

---
+++ Assets/DialogueControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueControl : MonoBehaviour
+{
+    public static DialogueControl main;
+
+    public TextMeshProUGUI dialogueBodyText;
+    public TextMeshProUGUI dialogueTitleText;
+    public Menu dialogueMenuParent;
+
+    public DialogueLine currentLine;
+    private int currentLineProgress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //singleton
+        if (main != null) Debug.LogError("Two Dialogue Controlls");
+        main = this;
+
+
+    }
+
+	public void AdvanceDialogue()
+	{
+        currentLineProgress++;
+        UpdateDialogue();
+        print("advanced dialogue");
+	}
+
+    public void UpdateDialogue()
+	{
+        if(currentLine != null && currentLineProgress < currentLine.parts.Count)
+		{
+            dialogueMenuParent.TryActivateMenu();//.SetActive(true);
+            dialogueBodyText.text = currentLine.parts[currentLineProgress].text;
+            dialogueTitleText.text = currentLine.title;
+		}
+		else
+		{
+            //call OnFinish
+            if (currentLine.OnFinish != null)
+            {
+                if (currentLineProgress >= currentLine.parts.Count)
+                {
+                    currentLine.OnFinish(true);//succeeded
+                }else{
+                    currentLine.OnFinish(false);//failed to finish dialogue
+				}
+            }
+
+            dialogueMenuParent.TryDeactivateMenu();//.SetActive(false);
+		}       
+	}
+
+    public void StartDialogueLine(DialogueLine line)
+	{
+        currentLineProgress = 0;
+        currentLine = line;
+        UpdateDialogue();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogueMenuParent.gameObject.activeSelf && (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)))
+		{
+            AdvanceDialogue();
+		}
+    }
+}
+
+[System.Serializable]
+public class DialoguePart
+{
+    [TextArea(1, 5)]
+    public string text;
+}
+
+public delegate void DialogueResult(bool succeeded);
+
+[System.Serializable]
+public class DialogueLine
+{
+    public string title;
+    public List<DialoguePart> parts;
+    public DialogueResult OnFinish;
+}
---
+++ Assets/DialogueOnClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueOnClick : MonoBehaviour, IMouseHoverable
+{
+	public GameObject onHover;
+	public DialogueLine dialogue;
+
+	private void Start()
+	{
+		if(onHover != null) onHover.SetActive(false);
+	}
+
+	public void OnMouseHoverFromRaycast()
+	{
+		if (onHover != null) onHover.SetActive(true);
+		if (Input.GetMouseButtonDown(0))
+		{
+			DialogueControl.main.StartDialogueLine(dialogue);
+		}
+	}
+
+	public void OnMouseStopHoverFromRaycast()
+	{
+		if (onHover != null) onHover.SetActive(false);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
