3c74d8450ed9bf5781503c7d0ac9653821bce5ef
RishiAstra <40644327+RishiAstra@users.noreply.github.com>
2021-05-09 16:40:03 -0400

    adventurers guild building

---
+++ Assets/Scripts/Editor/EditorDeselection.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class EditorDeselection : ScriptableObject
+{
+    [MenuItem("GameObject/Deselect all %#D")]
+    static void DeselectAll()
+    {
+        Selection.objects = new Object[0];
+    }
+}
