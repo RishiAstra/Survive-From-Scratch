ace66260bb7db6a13696ee47eb94e47f45a28ab7
Rishi Astra <40644327+RishiAstra@users.noreply.github.com>
2021-05-13 13:14:37 -0400

    added minimap

---
+++ Assets/MapCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class MapCamera : MonoBehaviour
+{
+    [Range(1, 25)] public float fps;
+    public Camera cam;
+    public Shader unlit;
+
+    private double ps;
+    private Stopwatch sw;
+    private Transform mainCameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sw = new Stopwatch();
+        sw.Start();
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = mainCameraTransform.position;
+        pos.y = transform.position.y;
+        transform.position = pos;
+
+        double ticks = sw.ElapsedTicks;
+        double seconds = ticks / Stopwatch.Frequency;
+
+		double timePerFrame = 1.0 / fps;
+		if (seconds - ps > timePerFrame)
+		{
+			ps += timePerFrame;
+            cam.RenderWithShader(unlit, "");
+		}
+    }
+}
