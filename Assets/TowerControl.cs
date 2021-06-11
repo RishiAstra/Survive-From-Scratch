76d90ef19b2c16887a3977b8773ec925232d9be2
Rishi Astra <40644327+RishiAstra@users.noreply.github.com>
2021-06-10 20:33:21 -0400

    adding tower menu for enterint towers

---
+++ Assets/TowerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerControl : MonoBehaviour
+{
+    public static TowerControl main;
+
+    public Menu towerMenu;
+    public GameObject towerLevelButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (main != null) Debug.LogError("two TowerControl");
+        main = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TowerTeleporter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerTeleporter : MonoBehaviour, IMouseHoverable
+{
+    public Tower t;
+    public GameObject onHover;
+
+	public void OnMouseHoverFromRaycast()
+	{
+        onHover.SetActive(true);
+
+		if (InputControl.InteractKeyDown())
+		{
+            TowerControl.main.towerMenu.ToggleMenu();
+		}
+	}
+
+	public void OnMouseStopHoverFromRaycast()
+	{
+        onHover.SetActive(false);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TowerControl.main.towerMenu.gameObject.activeSelf && InputControl.InteractKeyDown())
+		{
+            TowerControl.main.towerMenu.TryDeactivateMenu();
+		}
+    }
+}
+
+[System.Serializable]
+public class Tower
+{
+    public string name;
+    public List<bool> unlockedLevels;
+
+    public Tower(string name, int floorCount)
+	{
+        this.name = name;
+        this.unlockedLevels = new List<bool>();
+        for(int i = 0; i < floorCount; i++)
+		{
+            unlockedLevels.Add(false);
+		}
+	}
+
+    public string GetLevelSceneName(int level)
+	{
+        return name + level;
+	}
+
+}
