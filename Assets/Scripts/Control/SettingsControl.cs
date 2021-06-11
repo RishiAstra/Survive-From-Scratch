308d2943b72df32fc38e842276f30cb41000e21e
Rishi Astra <40644327+RishiAstra@users.noreply.github.com>
2021-06-11 18:54:25 -0400

    added bloom to settings

--- Assets/Scripts/Control/SettingsControl.cs
+++ Assets/Scripts/Control/SettingsControl.cs
@@ -11,10 +11,24 @@ using UnityEngine.Rendering;
 
 public class SettingsControl : MonoBehaviour
 {
+
+    public Volume mainPostProcessingVolume;
+    [Space(10)]
     public TMP_Dropdown presetDropdown;
+    [Space(10)]
+    public Toggle bloomEnableToggle;
+    public Slider bloomIntensitySlider;
+    public TMP_InputField bloomIntensityField;
+    //public Slider bloomThresholdSlider;
+    //public Slider bloomThresholdSlider;
+
+    private UnityEngine.Rendering.Universal.Bloom bloom;
     // Start is called before the first frame update
     void Start()
     {
+        mainPostProcessingVolume.profile.TryGet(out bloom);
+
+
         presetDropdown.options = new List<TMP_Dropdown.OptionData>();
 		string[] names = QualitySettings.names;
 		for (int i = 0; i < names.Length; i++)
@@ -23,6 +37,22 @@ public class SettingsControl : MonoBehaviour
 		}
 
         presetDropdown.onValueChanged.AddListener(OnQualityPresetSelected);
+
+        OnQualityPresetSelected(0);//load 1st quality
+
+        bloomEnableToggle.onValueChanged.AddListener((bool value) => { bloom.active = value; });
+        bloomIntensitySlider.onValueChanged.AddListener((float value) => { bloom.intensity.value = value; bloomIntensityField.text = value.ToString("F2"); });
+        bloomIntensityField.onValueChanged.AddListener((string text) => 
+            {
+                float value;
+                if(float.TryParse(text, out value))
+				{
+                    value = Mathf.Clamp(value, bloomIntensitySlider.minValue, bloomIntensitySlider.maxValue);
+                    bloom.intensity.value = value;
+                    bloomIntensitySlider.value = value;
+				}
+            }
+        );
     }
 
     void OnQualityPresetSelected(int index)
