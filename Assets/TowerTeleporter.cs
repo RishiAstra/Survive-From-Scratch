197d30c5bf4f711bef842ebda1102d9b190285bb
Rishi Astra <40644327+RishiAstra@users.noreply.github.com>
2021-06-11 08:52:37 -0400

    renamed tower1 and small fixes

--- Assets/TowerTeleporter.cs
+++ Assets/TowerTeleporter.cs
@@ -4,66 +4,67 @@ using UnityEngine;
 
 public class TowerTeleporter : MonoBehaviour, IMouseHoverable
 {
-    public Tower t;
-    public GameObject onHover;
+	public Tower t;
+	public GameObject onHover;
 
 	public void OnMouseHoverFromRaycast()
 	{
-        onHover.SetActive(true);
+		onHover.SetActive(true);
 
 		if (InputControl.InteractKeyDown())
 		{
-            TowerControl.main.SetTowerSelected(t);
-            TowerControl.main.towerMenu.TryActivateMenu();
+			TowerControl.main.SetTowerSelected(t);
+			TowerControl.main.SetButtonIndexSelected(0);
+			TowerControl.main.towerMenu.TryActivateMenu();
 		}
 	}
 
 	public void OnMouseStopHoverFromRaycast()
 	{
-        onHover.SetActive(false);
+		onHover.SetActive(false);
 	}
 
 	// Start is called before the first frame update
 	void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(TowerControl.main.towerMenu.gameObject.activeSelf && InputControl.InteractKeyDown())
-		{
-            TowerControl.main.towerMenu.TryDeactivateMenu();
-		}
-    }
-
-    void OnValidate()
 	{
-        if (t.unlockedLevels.Count == 0) t.unlockedLevels.Add(true);
-        t.unlockedLevels[0] = true;
+		onHover.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(TowerControl.main.towerMenu.gameObject.activeSelf && InputControl.InteractKeyDown())
+		{
+			TowerControl.main.towerMenu.TryDeactivateMenu();
+		}
+	}
+
+	void OnValidate()
+	{
+		if (t.unlockedLevels.Count == 0) t.unlockedLevels.Add(true);
+		t.unlockedLevels[0] = true;
 	}
 }
 
 [System.Serializable]
 public class Tower
 {
-    public string name;
-    public List<bool> unlockedLevels;
+	public string name;
+	public List<bool> unlockedLevels;
 
-    public Tower(string name, int floorCount)
+	public Tower(string name, int floorCount)
 	{
-        this.name = name;
-        this.unlockedLevels = new List<bool>();
-        for(int i = 0; i < floorCount; i++)
+		this.name = name;
+		this.unlockedLevels = new List<bool>();
+		for(int i = 0; i < floorCount; i++)
 		{
-            unlockedLevels.Add(false);
+			unlockedLevels.Add(false);
 		}
 	}
 
-    public string GetLevelSceneName(int level)
+	public string GetLevelSceneName(int level)
 	{
-        return name + level;
+		return name + level;
 	}
 
 }
