c18688f3d38c5ce48c7691f439bc2eb32407b791
Rishi Astra <40644327+RishiAstra@users.noreply.github.com>
2021-06-11 20:09:23 -0400

    added grass density to settings and adjusted bloom (removed HDR)

--- Assets/Scripts/Control/SettingsControl.cs
+++ Assets/Scripts/Control/SettingsControl.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using AwesomeTechnologies.VegetationStudio;
+using AwesomeTechnologies;
 
 public class SettingsControl : MonoBehaviour
 {
@@ -19,15 +22,18 @@ public class SettingsControl : MonoBehaviour
     public Toggle bloomEnableToggle;
     public Slider bloomIntensitySlider;
     public TMP_InputField bloomIntensityField;
+    public Slider grassDensitySlider;
+    public TMP_InputField grassDensityField;
     //public Slider bloomThresholdSlider;
     //public Slider bloomThresholdSlider;
 
     private UnityEngine.Rendering.Universal.Bloom bloom;
+    private 
     // Start is called before the first frame update
     void Start()
     {
         mainPostProcessingVolume.profile.TryGet(out bloom);
-
+        SceneManager.sceneLoaded += OnSceneLoaded;
 
         presetDropdown.options = new List<TMP_Dropdown.OptionData>();
 		string[] names = QualitySettings.names;
@@ -40,8 +46,11 @@ public class SettingsControl : MonoBehaviour
 
         OnQualityPresetSelected(0);//load 1st quality
 
+        bloomEnableToggle.isOn = bloom.active;
         bloomEnableToggle.onValueChanged.AddListener((bool value) => { bloom.active = value; });
+        bloomIntensitySlider.value = bloom.intensity.value;
         bloomIntensitySlider.onValueChanged.AddListener((float value) => { bloom.intensity.value = value; bloomIntensityField.text = value.ToString("F2"); });
+        bloomIntensityField.text = bloom.intensity.value.ToString("F2");
         bloomIntensityField.onValueChanged.AddListener((string text) => 
             {
                 float value;
@@ -53,9 +62,41 @@ public class SettingsControl : MonoBehaviour
 				}
             }
         );
+
+        grassDensitySlider.onValueChanged.AddListener((float value) => { UpdateGrassDensity(); grassDensityField.text = value.ToString("F2"); });
+        grassDensityField.onValueChanged.AddListener((string text) =>
+        {
+            float value;
+            if (float.TryParse(text, out value))
+            {
+                value = Mathf.Clamp(value, grassDensitySlider.minValue, grassDensitySlider.maxValue);
+                grassDensitySlider.value = value;
+                UpdateGrassDensity();
+            }
+        }
+        );
     }
 
-    void OnQualityPresetSelected(int index)
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		UpdateGrassDensity();
+	}
+
+	private void UpdateGrassDensity()
+	{
+		VegetationStudioManager m = FindObjectOfType<VegetationStudioManager>();
+        if(m != null)
+		{
+            foreach (VegetationSystem v in m.VegetationSystemList)
+		    {
+			    v.SetGrassDensity(grassDensitySlider.value);
+                v.SetPlantDensity(grassDensitySlider.value);
+		    }
+		}
+		
+	}
+
+	void OnQualityPresetSelected(int index)
 	{
         QualitySettings.SetQualityLevel(index);
         GraphicsSettings.renderPipelineAsset = QualitySettings.GetRenderPipelineAssetAt(index);
